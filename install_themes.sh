68f140c50c7721abc98f6550d4e5f49dc0289fa3
deodato_bastos <deodatobasto@gmail.com>
2022-04-16 22:00:48 -0300

    refctor: update ppa to downloads themes directly

--- install_themes.sh
+++ install_themes.sh
@@ -2,13 +2,8 @@
 
 sudo apt install gnome-tweak-tool fonts-hack-ttf fonts-firacode -y
 
-cd ~
-mkdir themes && cd themes
-
-git clone https://github.com/daniruiz/flat-remix
-git clone https://github.com/daniruiz/flat-remix-gtk
-
-mkdir -p ~/.icons && mkdir -p ~/.themes
-cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/themes/Flat-Remix-GTK* ~/.themes/
+sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo apt update
+sudo apt install flat-remix-gnome
 
 echo "themes and fonts installed successfully \n"
