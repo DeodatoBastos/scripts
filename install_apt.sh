1e0db75330435b4ebb1c3ec3faa1364c3b7979e4
deodato_bastos <deodatobasto@gmail.com>
2022-04-27 08:17:54 -0300

    fix gramar error and add more apps to pos-install

--- install_apt.sh
+++ install_apt.sh
@@ -17,10 +17,17 @@ sleep 2
 echo "installing flathub"
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.Inkscape.Inkscape
-flatpak install flathub org.gimp.GIMP
+# flatpak install flathub org.Inkscape.Inkscape
 
 echo "flakpak and flathub installed succefully \n"
+
+flatpak install flathub org.gimp.GIMP
+
+echo "GIMP installed successfully \n"
+
+flatpak install flathub com.github.marktext.marktext
+
+echo "MarkText installed successfully \n"
 
 sudo apt install VLC
 
@@ -30,20 +37,24 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt install code
-
-echo "VSCode installed successfully \n"
-
 sudo apt install flameshot
 
-echo "Flame Shot installed successfully \n"
+echo "FlameShot installed successfully \n"
 
 sudo add-apt-repository ppa:bashtop-monitor/bashtop
 sudo apt update
 sudo apt install bashtop
 
-echo "BashTop installed successfully \n"
+echo "Bashtop installed successfully \n"
+
+sudo apt install htop
+
+echo "Htop installed successfully \n"
 
 sudo apt install neofetch
 
-echo "Neofetch installed successfully \n "
+echo "Neofetch installed successfully \n"
+
+sudo apt install preload
+
+echo "Preload installed successfully \n"
