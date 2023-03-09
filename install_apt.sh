9ebb0e79130500375796943b4d94d462efa226fd
DeodatoBastos <deodatobasto@gmail.com>
2023-03-08 22:53:44 -0300

    add zsh pluggins into scritp

--- install_apt.sh
+++ install_apt.sh
@@ -95,6 +95,23 @@ echo -e "zsh installed successfully "
 chsh -s /usr/bin/zsh
 echo -e $SHELL
 
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+sudo apt update
+sudo apt install zsh-autosuggestions
+
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-completions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-completions.gpg > /dev/null
+sudo apt update
+sudo apt install zsh-completions
+
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-syntax-highlighting/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-syntax-highlighting.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-syntax-highlighting/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-syntax-highlighting.gpg > /dev/null
+sudo apt update
+sudo apt install zsh-syntax-highlighting
+
+echo -e "zsh plugins installed successfully "
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo -e 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo -e "powerlevel10k installed successfully  to configure type: 'pk10 configure' "
